c7782e2fcaca83dff4794f5e690ca9feae5e6d6c
Kaloian Kozlev <kbkozlev@gmail.com>
2023-12-22 01:49:15 +0200

    added Knizhen Pazar

--- scraper/main.py
+++ scraper/main.py
@@ -1,8 +1,10 @@
 from helikon import HelikonScraper
 from orange import OrangeScraper
+from knizhenPazar import PazarScraper
 
-search_term = 'Хари Потър и стаята на тайните'
+search_term = ('Един наивник на средна възраст. Реквием за една мръсница')
 
 if __name__ == "__main__":
-    HelikonScraper(search_term)
-    OrangeScraper(search_term)
+    #HelikonScraper(search_term)
+    #OrangeScraper(search_term)
+    PazarScraper(search_term)
