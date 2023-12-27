acff082a7b823f76a11c623ec73ac0b4f4e20fb7
Kaloian Kozlev <kbkozlev@gmail.com>
2023-12-27 15:14:15 +0200

    reformated code

--- main.py
+++ main.py
@@ -2,38 +2,35 @@ from scrapers.helikon import HelikonScraper
 from scrapers.orange import OrangeScraper
 from scrapers.knizhenPazar import PazarScraper
 
-search_term = 'Сойка присмехулка'
 
+class BookScraperManager:
+    def __init__(self, *scrapers):
+        self.scrapers = list(scrapers)
 
-def print_books(*scrapers):
-    all_books = []
+    def fetch_books(self):
+        all_books = []
 
-    for scraper in scrapers:
-        try:
-            books = scraper.get_book_list()
-            all_books.extend(books)
-        except Exception as e:
-            print(f"Error fetching books from {type(scraper).__name__}: {e}")
+        for scraper in self.scrapers:
+            try:
+                all_books.extend(scraper.get_book_list())
+            except Exception as e:
+                print(f"Error fetching books from {type(scraper).__name__}: {e}")
 
-    if all_books:
-        max_key_length = max(len(key) for book in all_books for key in book.keys())
-
-        for idx, book in enumerate(all_books, start=1):
-            print(f'Book Nr.{idx}')
-            print('------------')
-            for key, value in book.items():
-                print(f"{key.ljust(max_key_length)}: {value}")
-            print()
-
-        print(f"Total books fetched: {len(all_books)}")
-    else:
-        print("No books fetched from any scraper.")
-    print("==================================")
+        return all_books
 
 
 if __name__ == "__main__":
-    helikon = HelikonScraper(search_term)
-    orange = OrangeScraper(search_term)
+    search_term = 'стаята на тайните'
+
+    helikon = HelikonScraper(search_term),
+    orange = OrangeScraper(search_term),
     pazar = PazarScraper(search_term)
 
-    print_books(helikon, orange, pazar)
+    scraper_manager = BookScraperManager(helikon, orange, pazar)
+    books = scraper_manager.fetch_books()
+
+    for i, book in enumerate(books, start=1):
+        print(f"Book {i}:")
+        for key, value in book.items():
+            print(f'{key}: {value}')
+        print()
