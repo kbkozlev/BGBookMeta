c6467fd970f48146ca4556720f8785e7eb56795c
Kaloian Kozlev <kbkozlev@gmail.com>
2023-12-28 20:37:07 +0200

    Added biblioman.py scraper

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from scrapers.helikon import HelikonScraper
 from scrapers.orange import OrangeScraper
 from scrapers.knizhenPazar import PazarScraper
+from scrapers.biblioman import BibliomanScraper
 import concurrent.futures
 
 
@@ -16,24 +17,27 @@ def fetch_books_from_scraper(scraper):
 if __name__ == "__main__":
     start_time = time.time()  # Record the start time
 
-    search_term = 'Списъкът "Октомври"'
+    search_term = 'Хари Потър и стаята на тайните'
 
     helikon = HelikonScraper(search_term)
     orange = OrangeScraper(search_term)
     pazar = PazarScraper(search_term)
+    biblioman = BibliomanScraper(search_term)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
         # Submitting tasks for execution
         future_helikon = executor.submit(fetch_books_from_scraper, helikon)
         future_orange = executor.submit(fetch_books_from_scraper, orange)
         future_pazar = executor.submit(fetch_books_from_scraper, pazar)
+        future_biblioman = executor.submit(fetch_books_from_scraper, biblioman)
 
         # Retrieve results
         books_from_helikon = future_helikon.result()
         books_from_orange = future_orange.result()
         books_from_pazar = future_pazar.result()
+        books_from_biblioman = future_biblioman.result()
 
-    all_books = books_from_helikon + books_from_orange + books_from_pazar
+    all_books = books_from_helikon + books_from_orange + books_from_pazar + books_from_biblioman
 
     for i, book in enumerate(all_books, start=1):
         print(f"Book {i}:")
---
+++ scrapers/biblioman.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+import cloudscraper
+from .helpers.utils import format_book_details, process_title_text
+
+
+class BibliomanScraper:
+    def __init__(self, search_term):
+        self.formatted_details = []
+        self.individual_search_items = set()
+        self.scraper = cloudscraper.create_scraper(
+            browser={'browser': 'firefox', 'platform': 'windows', 'mobile': False}
+        )
+        url = f"https://biblioman.chitanka.info/books?q={search_term}"
+        response = self.scraper.get(url)
+
+        if response.status_code == 200:
+            html_content = response.content
+            self.__extract_individual_search_items(html_content, search_term)
+            self.__get_book_info()
+        else:
+            print(f"{self.__class__.__name__} failed to fetch the main page: {response.status_code}")
+
+    def __extract_individual_search_items(self, html_content, search_term):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        book_items = soup.select("div.col-xs-12.col-md-6")
+
+        for item in book_items:
+            a_tag = item.find('div', class_='book-title-and-author').find('a')
+            processed_title = process_title_text(a_tag.text.strip())
+            search = process_title_text(search_term)
+
+            if search in processed_title:
+                href = a_tag['href']
+                self.individual_search_items.add(href)
+
+    def __get_book_info(self):
+
+        for item in self.individual_search_items:
+            url = f"https://biblioman.chitanka.info{item}"
+            response = self.scraper.get(url)
+
+            if response.status_code == 200:
+                html_content = response.content
+                soup = BeautifulSoup(html_content, 'html.parser')
+
+                book_details = {
+                    "book_title": soup.find(class_='col-md-8 entity-field entity-field-title').find('a').text.strip(),
+                    "author": soup.find(class_='col-md-8 entity-field entity-field-author').find('a').text.rstrip(),
+                    "img_src": "https://biblioman.chitanka.info/" +
+                               soup.find('a', class_='thumb-link')['href'].split('.jpg')[
+                                   0] + '.jpg',
+                    "description": ''.join(p.get_text(strip=True) for p in
+                                           soup.find(class_='col-md-8 entity-field entity-field-annotation').find_all(
+                                               'p')),
+                    "publisher": soup.find(class_='col-md-8 entity-field entity-field-publisher').find(
+                        'a').text.strip(),
+                    "language": soup.find(class_='col-md-8 entity-field entity-field-language').find('a').text.strip(),
+                    "publication_year": soup.find(class_='col-md-8 entity-field entity-field-publishingYear').find(
+                        'a').text.strip(),
+                    "ISBN": soup.find(class_='col-md-8 entity-field entity-field-isbn').find('a').text.replace('-', '').strip(),
+                    "tags": soup.find(class_='col-md-8 entity-field entity-field-category').find('a').text.strip(),
+                    }
+
+                self.__format_book_details(book_details)
+
+            else:
+                print(f"{self.__class__.__name__} failed to fetch {url}: {response.status_code}")
+
+    def __format_book_details(self, book_details):
+        formatted_details = format_book_details(book_details)
+        self.formatted_details.append(formatted_details)
+
+    def get_book_list(self):
+        return self.formatted_details
